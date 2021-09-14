9c031edcd1bdd86d92df11e7bc39fca78ae31f89
Navya-06 <chowdarynavya1999@gmail.com>
2021-09-14 18:11:23 -0400

    vowels or consonants

---
+++ Navya/src/BasicAssignment/VowelsOrConsonants.java
@@ -0,0 +1,18 @@
+package BasicAssignment;
+
+import java.util.Scanner;
+
+public class VowelsOrConsonants {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        char ch = myObj.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch =='u'){
+            System.out.println(ch+" is an lowercase Vowels");
+        }
+            else if(ch == 'A' || ch == 'E' || ch == 'I' || ch =='O' || ch == 'U' ){
+                System.out.println(ch+ " is an uppercase vowels");
+            }
+            else 
+            System.out.println(ch +" is a consonants");
+    }
+}
