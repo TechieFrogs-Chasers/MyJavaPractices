b715a7e313a168d6c986df39162bd9ae25c3055b
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-09-14 18:13:12 +0530

    validation method for adding two integers

--- sindhu/src/Assignment1/Addtwointegers.java
+++ sindhu/src/Assignment1/Addtwointegers.java
@@ -7,7 +7,7 @@ public class Addtwointegers {
         int a,b;
         Scanner input = new Scanner(System.in);
 
-        //validation for first integer
+        //validation method for first integer
 
         do{
             System.out.println("Enter first  integer");
@@ -19,13 +19,13 @@ public class Addtwointegers {
              a = input.nextInt();
            } while (a<=0);
     
-        //validation for the second integer
+        //validation method for the second integer
         
            do{
             System.out.println("Enter second  integer");
            while (!input.hasNextInt() ){
-               String y = input.next();
-               System.out.println(input+"  is not a valid type");
+             String y = input.next();
+               System.out.println(y+"  is not a valid type");
              }
              b = input.nextInt();
            } while (b<=0);
