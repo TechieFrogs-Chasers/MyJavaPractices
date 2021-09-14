05065e1855e54e0558482b26eaa8f57d4b862eba
Tulasikakara <tulasi.polina@gmail.com>
2021-09-14 01:38:12 -0400

    changes done

--- Tulasi-Java/src/Basicassignments/ComputeQandR.java
+++ Tulasi-Java/src/Basicassignments/ComputeQandR.java
@@ -7,29 +7,32 @@ public class ComputeQandR {
         Scanner myscannerobj = new Scanner(System.in);//Scanner
         int dividend, divisor;
         System.out.println("Enter first value: ");
-        dividend = myscannerobj.nextInt();
 
-        if (dividend <= 0) {//Validation
-            System.out.println("Please enter a positive number! ");
+        do {//Validation
+
             while (!myscannerobj.hasNextInt()) {
                 String input = myscannerobj.next();
                 System.out.println("That's not a number!");
             }
             dividend = myscannerobj.nextInt();
+            if (dividend < 0)
+                System.out.println("Please enter a positive number! ");
         }
-        while (dividend <= 0) ;
-        System.out.println("Enter second value: ");
-        divisor = myscannerobj.nextInt();
+        while (dividend < 0);
 
-        if (divisor <= 0) {//Validation
-            System.out.println("Please enter a positive number! ");
+        System.out.println("Enter second value: ");
+
+        do {//Validation
+
             while (!myscannerobj.hasNextInt()) {
                 String input = myscannerobj.next();
                 System.out.println("That's not a number!");
             }
             divisor = myscannerobj.nextInt();
+            if (divisor < 0)
+                System.out.println("Please enter a positive number! ");
         }
-        while (divisor <= 0) ;
+        while (divisor < 0);
         System.out.println("Quotient = " + dividend / divisor);
         System.out.println("Remainder = " + dividend % divisor);
         myscannerobj.close();//Scanner Close
