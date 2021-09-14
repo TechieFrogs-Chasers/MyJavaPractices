680df1b4394da5bcda68d6baf7cde4b9fb181997
Battineni-H <hemabattineni14@gmail.com>
2021-09-14 11:41:24 -0500

    hemalatha

---
+++ hemalatha/src/Assignment_1/QuoRem.java
@@ -0,0 +1,28 @@
+package Assignment_1;
+
+import java.util.Scanner;
+public class QuoRem {
+    public static void main(String[] args)
+    {
+        //int a=3,b=9,rem,quo;
+       // System.out.println("enter the number");
+       // rem = a/b;
+       // quo = a%b;
+        //System.out.println("values  "  +rem+ " " +quo);
+        //using scanner
+        Scanner in = new Scanner(System.in);//for input
+        System.out.println("enter the value to x:  ");
+        int x = in.nextInt();
+        System.out.println("enter the value to y:  ");
+        int y = in.nextInt();
+        int i = x/y;//to find quotient
+        int j = x%y;// to find remainder
+        System.out.println("enter Quotient is : " +i);
+        System.out.println("enter remainder is : " +j);
+
+
+
+
+
+    }
+}
