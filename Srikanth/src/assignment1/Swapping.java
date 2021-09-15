9225794d10db05bb954f01d12205a519a12c8dc3
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-15 20:40:00 +0530

    commit

--- Srikanth/src/assignment1/Swapping.java
+++ Srikanth/src/assignment1/Swapping.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Swapping {
+    public static void main(String[] args) {
+
+        int a, b;
+        Scanner swapObj = new Scanner(System.in);
+        System.out.println("Enter a value:");
+        a = swapObj.nextInt();
+        System.out.println("Enter b value:");
+        b = swapObj.nextInt();
+        System.out.println("Before swapping a: " + a + ", b:" + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping a:" + a + ", b:" + b);
+
+        swapObj.close();
+    }
 
 }
