3c68a5976c9d61f98cafbb4ae6e9726614144be7
umaboppana <mrsumaram@gmail.com>
2021-09-14 19:35:58 -0500

    my assignment

--- uma/src/JavaBasicsAssignments/VowelConsonant.java
+++ uma/src/JavaBasicsAssignments/VowelConsonant.java
@@ -6,29 +6,48 @@ public class VowelConsonant {
     public static void main(String[] args) {
 
         Scanner VowelConsonantobj = new Scanner(System.in);
-        String ch;
-        int num;
-        do{
-            System.out.println("enter your alphabet:");
-           while(!VowelConsonantobj.hasNext() ){
-               //String alpha=VowelConsonantobj.next();
-               int input=VowelConsonantobj.nextInt();
-               System.out.println(input+"  is not a valid type");
-             }
-             num=VowelConsonantobj.nextInt();
-           } while(num>=0);
 
+        char ch=VowelConsonantobj.next().charAt(0);
 
+       // char ch1=VowelConsonantobj.next().charAt(0);
+       char ch1='a';
+        
+    do{
+        System.out.println("Enter a character : ");
 
+       while((VowelConsonantobj.hasNextInt() )){
 
+           int num=VowelConsonantobj.nextInt();
 
+     if(num>=65 ||num<=90){
+         System.out.println("number is equivalent to alphabet");}
+         else{
+             System.out.println("not an alphabet");
+         }
+         
+     
+        switch(ch){
+        
+         case'a','e','i','o','u'->
+            System.out.println(" is a lower case vowel");
 
+        case 'A','E','I','O','U'->
+            System.out.println(" is a upper case vowel");
+        
 
-
-
-
+        default->
+            System.out.println(ch+"  is consonant");
+        }
+    } VowelConsonantobj.next();
+    //char ch1=VowelConsonantobj.next().charAt(0);
+        
+        }while(ch==ch1);
+        
 
      VowelConsonantobj.close();
+        
+
+
     }
     
 }
