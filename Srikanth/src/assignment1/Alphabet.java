013423f97742416ea0ca2b9bb73680c09e64c7d0
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-15 20:38:52 +0530

    changes

---
+++ Srikanth/src/assignment1/Alphabet.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        char character;
+        Scanner alphabet = new Scanner(System.in);
+    }
+}
