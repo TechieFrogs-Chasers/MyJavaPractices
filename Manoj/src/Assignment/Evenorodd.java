f43dc8bfee4d9b4ff87abde3fd538ceee03b4621
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-16 18:59:15 +0530

    commit

--- Manoj/src/Assignment/Evenorodd.java
+++ Manoj/src/Assignment/Evenorodd.java
@@ -1,3 +1,4 @@
+package Assignment;
 
 import java.util.Scanner;
 
@@ -8,22 +9,19 @@ public class Evenorodd {
         int num;
         do{
             System.out.println(" enter the number :");
-            while (!object.hasNextInt()); //here we checks the given value is number or not
+            while (!object.hasNextInt()) //here we checks the given value is number or not
             {
                 System.out.println(" this is not a number ; please enter the number ");
                 object.next();  
             } 
             num=object.nextInt();
-        }while (num%2==0);
-        System.out.println("the given number is even ");
+        }while (num<=0);
+
+            if (num%2==0){ // here it checks for the given value is divisible by 2 
+                System.out.println("given number is even "); // value divisible by 2 prints even 
+            }else 
+            System.out.println("given number is odd"); //value not divisible by 2 prints odd
         object.close();
-        
-
-        /* if (num%2==0){
-             System.out.println("given number is even ");
-         }else 
-         System.out.println("given number is odd");*/
-
 
     }
 }
