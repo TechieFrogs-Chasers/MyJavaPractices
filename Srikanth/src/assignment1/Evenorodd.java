27eb05f97aefd79c118231ea89be251fa0527060
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-16 08:04:39 +0530

    assignment

---
+++ Srikanth/src/assignment1/Evenorodd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Evenorodd {
+    public static void main(String[] args) {
+
+        Scanner Object = new Scanner(System.in);
+        int a;
+
+        do {
+            System.out.println("Enter the number");
+            while (!Object.hasNextInt()) {
+                String x = Object.next();
+                System.out.println(x + "  is not a valid type");
+            }
+            a = Object.nextInt();
+        } while (a <= 0);
+
+        String min = (a % 2 == 0) ? "even" : "odd";
+        System.out.println(min);
+        Object.close();
+    }
+}
