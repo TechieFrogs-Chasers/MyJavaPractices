bf48fbaf1c1b2168f827f08f985b04296543ed5c
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-18 00:38:35 +0530

    commit

--- Manoj/src/Assignment/Displaychar.java
+++ Manoj/src/Assignment/Displaychar.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class Displaychar {
     public static void main(String...args){
-        Scanner object= new Scanner(System.in);
+        Scanner object= new Scanner(System.in); //object created  using scanner 
         char ch;
-        do{
+        do{                                 //here we check the validation using so while 
             System.out.println("enter an charater ");
             while(object.hasNextInt())
             {
                 String s=object.next();
                 System.out.println( s+ "this is not a character please enter a valid one ");
-            }ch=object.next().charAt(0);
+            }
+            ch=object.next().charAt(0);
 
         }while(ch<=0);
-        object.close();
+        object.close(); //object closed 
          for (ch='A' ; ch<='Z'; ++ch){
              System.out.println(ch +"is displayed character  ");
          }
