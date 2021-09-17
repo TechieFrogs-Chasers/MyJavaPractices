4fc61068574b90ee8dee9bf7fcd7409e5465afa7
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-18 00:01:34 +0530

    commit

--- Manoj/src/Assignment/Alphabetornot.java
+++ Manoj/src/Assignment/Alphabetornot.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Alphabetornot {
     public static void main(String...args){
-        Scanner object= new Scanner(System.in);
+        Scanner object= new Scanner(System.in); // object is created using scanner 
         char ch;
-        do {
+        do {                        // here we check  the validation  using the do while 
             System.out.println("enter any alphabet ");
             while(object.hasNextInt())
             {
-                String n=object.next();
+                String n=object.next();     
                 System.out.println(n+ " this is not a alphabet .enter valid one ");
             }ch=object.next().charAt(0);
-        }while(ch<=0);
-        if ((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z')){
+        }while(ch<=0);  
+
+        if ((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z')){     // if else is used to check the allphabet or not 
             System.out.println( ch + " is an alphabet ");
         }
         else
