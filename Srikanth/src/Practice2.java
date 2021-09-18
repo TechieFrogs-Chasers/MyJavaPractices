3fdfe4f657baeaa0fd63a7d5b4d57e0e0b3c97ef
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-18 14:34:20 +0530

    commit

---
+++ Srikanth/src/Practice2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner Char = new Scanner(System.in);
+
+        System.out.println("Enter A value:");
+        char a = Char.nextInt();
+        // int a = 5;
+
+        System.out.println("Enter B value:");
+        char b = Char.nextInt();
+        // int b = 4;
+
+        char c = (char) (a + b);
+
+        System.out.println("c:" + c);
+        Char.close();
+    }
+    public class char{
+        
+    }
+}
