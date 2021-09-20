219b063d228b856ba2e8616533beeed6f1b07bc6
Veerababug <veerababug126@gmail.com>
2021-09-20 18:35:10 +0530

    commit

--- Veerababu/src/Assignment 1/Gcd.java
+++ Veerababu/src/Assignment 1/Gcd.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Assignment11 {
+public class Gcd {
     public static void main(String[] args) {
         Scanner GCD = new Scanner(System.in);
         System.out.println("Print the First Number");
