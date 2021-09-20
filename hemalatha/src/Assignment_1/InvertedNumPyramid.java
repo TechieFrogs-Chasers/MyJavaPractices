d79ec8a5a42844e362fb0f60067b6c1db13bef28
Battineni-H <hemabattineni14@gmail.com>
2021-09-20 09:18:00 -0500

    hemalatha

--- hemalatha/src/Assignment_1/InvertedNumPyramid.java
+++ hemalatha/src/Assignment_1/InvertedNumPyramid.java
@@ -5,8 +5,8 @@ public class InvertedNumPyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number of rows: ");
-        int rows=sc.nextInt();
-        for(int i=rows;i>=1;i--)
+        int value=sc.nextInt();
+        for(int i=value;i>=1;i--)
         {
             for(int j=1;j<=i;j++)
             {
