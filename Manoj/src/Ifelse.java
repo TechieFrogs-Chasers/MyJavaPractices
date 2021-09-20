46476a6169b3fb74375fbd0ae8226a1615614d5d
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-20 17:52:40 +0530

    commit

---
+++ Manoj/src/Ifelse.java
@@ -0,0 +1,22 @@
+public class Ifelse {
+    public static void main(String...args){
+      /*  int a=3;
+        int b=5;
+        if(a<b){
+            System.out.println("a is greater ");
+        }
+        else{
+            System.out.println("b is lesser");
+        }*/
+       
+        int a=4;
+        if (a%2==0){
+            System.out.println("given number is even ");
+        }
+        else{
+            System.out.println("given number is odd");
+            
+        }
+    }
+    
+}
