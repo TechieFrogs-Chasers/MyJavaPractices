eb867ca45a516ac9baac98c8b1a7857690563288
Tulasikakara <tulasi.polina@gmail.com>
2021-09-20 22:27:40 -0400

    changes done

---
+++ Tulasi-Java/src/Practices/Hello.java
@@ -0,0 +1,15 @@
+package Practices;
+
+public class Hello {
+    static public void main(String... args) {
+        System.out.println("Hello coding!!");
+       // System.out.println(args[0]);
+        // System.out.println(args[1]);
+        int x=234;int y=-234;
+        //System.out.println(x++);//unary-postfix
+        //System.out.println(x);
+        //System.out.println(++x);//unary-prefix
+        System.out.println(~x);//unary-oppsitie
+        System.out.println(~y);//unary-oppsitie
+    }
+}
