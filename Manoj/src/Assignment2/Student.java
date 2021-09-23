b1a4844ef8942228a655541d65ad8c7c4c707b6c
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-23 18:27:27 +0530

    commit

--- Manoj/src/Assignment2/Student.java
+++ Manoj/src/Assignment2/Student.java
@@ -1,13 +1,13 @@
 package Assignment2;
 
-public class Student {
+public class Student {  //class name as student
     int rollno;
     String name;
     public static void main(String...args){
-        Student object=new Student(2,"john");
+        Student object=new Student(2,"john");//we created a object 
 
     }
-   Student(int i,String n){
+   Student(int i,String n){ //constructor with parameters
        rollno=i;
        name=n;
        System.out.println("name "+ name+" rollno "+rollno);
