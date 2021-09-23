dad353f281f477fc1963978e970ed631ecfd9b4d
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-23 08:50:46 +0530

    changes made

---
+++ Srikanth/src/practice/HeadPhones.java
@@ -0,0 +1,60 @@
+package practice;
+
+public class HeadPhones {
+
+    String color = "red/blue";
+    String charge = "usb c";
+    String[] controls = { "power", "vloume", "skip" };
+
+    static boolean power = false;
+    static int volume = 0;
+
+    public static void poweron() {
+        power = true;
+    }
+
+    public static void poweroff() {
+        power = false;
+        System.out.println();
+    }
+
+    public static class Constructor {
+        Constructor() {
+            System.out.println(" Its driver class.");
+        }
+    }
+
+    public void charge() {
+        System.out.println("Usb-c");
+    }
+
+    public void color() {
+        System.out.println("red/blue");
+    }
+
+}
+
+class information {
+
+    void companyname() {
+        System.out.println("Enter brand name: ");
+    }
+
+    void price() {
+        System.out.println("Enter the value: ");
+    }
+
+    information() {
+        System.out.println("This is a default constructor.");
+    }
+
+    public static void main(String[] args) {
+        HeadPhones myObjHeadPhones = new HeadPhones();
+        myObjHeadPhones.charge(); // non-static
+        myObjHeadPhones.color(); // non-static
+        Pen myObjectPen = new Pen();
+        Pen.twist(); // this is a static variable
+        System.out.println(Pen.twisted);
+        myObjectPen.color(); // this is non-static variable
+    }
+}
---
+++ Srikanth/src/practice/Practice.java
@@ -0,0 +1,34 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = 0;
+
+        System.out.println("Enter a value: ");
+
+        a = input.nextInt();
+
+        System.out.println("Entered value: " + a);
+        input.close();
+    }
+
+    static int validate(Scanner input) {
+        int num;
+
+        do {
+            while (input.hasNextInt()) {
+                System.out.println("Enter a value:");
+                input.next();
+                System.out.println("This is not correct:");
+            }
+            num = input.nextInt();
+        } while (num >= 0);
+        return num;
+
+    }
+}
