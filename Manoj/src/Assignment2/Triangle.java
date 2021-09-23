5d0b205213edcd654ecd36be98d8f15743342143
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-23 18:27:16 +0530

    commit

---
+++ Manoj/src/Assignment2/Triangle.java
@@ -0,0 +1,24 @@
+package Assignment2;
+
+public class Triangle {
+    int a,b,c;
+    public static void main(String...args){
+        Triangle obj=new Triangle();      //object has created 
+        Triangle obj2=new Triangle(3,4,5);
+    }
+    Triangle(){ //without parameters 
+        a=3;
+        b=4;
+        c=5;
+        System.out.println("perimeter of triangle is");
+        System.out.println(a+b+c);
+    }
+    Triangle(int i,int j,int k){ //with parameters 
+      a=i;
+      b=j;
+      c=k;
+      System.out.println("perimeter of triangle");
+      System.out.println(a+b+c);
+    }
+    
+}
