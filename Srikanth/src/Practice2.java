cf9629b947388de387034fedc7608f536a8eaa78
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-23 08:42:34 +0530

    changes made

--- Srikanth/src/Practice2.java
+++ Srikanth/src/Practice2.java
@@ -2,22 +2,19 @@ import java.util.*;
 
 public class Practice2 {
     public static void main(String[] args) {
-        Scanner Char = new Scanner(System.in);
+        Scanner character = new Scanner(System.in);
 
         System.out.println("Enter A value:");
-        char a = Char.nextInt();
+        int a = character.nextInt();
         // int a = 5;
 
         System.out.println("Enter B value:");
-        char b = Char.nextInt();
+        int b = character.nextInt();
         // int b = 4;
 
         char c = (char) (a + b);
 
         System.out.println("c:" + c);
-        Char.close();
-    }
-    public class char{
-        
+        character.close();
     }
 }
