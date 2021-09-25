9659808a044a81b1636a461be775432676b78ec1
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-25 18:35:39 +0530

    commit

--- Manoj/src/Assignment2/Area.java
+++ Manoj/src/Assignment2/Area.java
@@ -14,23 +14,37 @@ public class Area {
     }
     public static void main(String...args){
         Scanner obj=new Scanner(System.in);
-        Area object=new Area();
-        Area object2=new Area();//
-        object.SetDim(5,6);     //we call the method setDim
+            Area object =new Area();
+            Area object2=new Area();
+            Area object3=new Area();
+        object.SetDim(5,8);     //we call the method setDim
         System.out.println("enter the length");
-        int l=obj.nextInt();
+           int l=obj.nextInt();
         System.out.println("enter the breadth");
-        int b=obj.nextInt();
+           int b=obj.nextInt();
         object2.getArea(l,b); //we call the second method of return type
+        object3.returnArea(l,b);
 
+      /*  System.out.println("enter the length");
+            int n=obj.nextInt();
+        System.out.println("enter the breadth");
+            int m=obj.nextInt();
+         object3.returnArea(n,m); */  // call the another method 
+         
+         
       obj.close();
     }
-    
-    public int getArea(int l ,int b){  //
+    public int getArea(int l ,int b){  //with return type
         int area;
         area=l*b;
         System.out.println("area of rectangle is:"+area);
         return area;
 
     } 
+    public int returnArea(int l, int b){
+        int area;
+        area =l*b;
+        System.out.println("area of rectangle for returnarea: "+ area);
+        return area;
+    }
 }
