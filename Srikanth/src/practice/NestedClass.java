a092e9f0212091d523ae88ead8226d68ca36258f
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-28 08:30:31 +0530

    still need to practice

--- Srikanth/src/practice/NestedClass.java
+++ Srikanth/src/practice/NestedClass.java
@@ -1,8 +1,30 @@
 package practice;
 
 public class NestedClass {
+    int i;
+    String house;
 
     private static class InnerClass {
 
+        public static void main(String[] args) {
+            NestedClass.InnerClass objInnerClass = new InnerClass();
+            objInnerClass.rooms();
+            // objInnerClass.House();
+        }
+
+        void rooms() {
+            System.out.println("This is inner room");
+        }
     }
+
+    void House() {
+        System.out.println("THis is outer House");
+    }
+
+    public static void main(String[] args) {
+        NestedClass objNestedClass = new NestedClass();
+        objNestedClass.House();
+        // objNestedClass.rooms();
+    }
+
 }
