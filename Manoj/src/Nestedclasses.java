705ab68d31df9ce22044f147efa76d59b341e489
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-28 23:20:50 +0530

    commit

--- Manoj/src/Nestedclasses.java
+++ Manoj/src/Nestedclasses.java
@@ -1,18 +1,20 @@
 public class Nestedclasses {
     int i;
-    String name ;
+    String name="manoj" ;
+    
 
     class Inner{       //inner class or nested class OR non static nested class
 
            class NestInner{      //created a nest inner method in the inner class 
                void NestInnerMethod(){
-                   System.out.println("NestInnerMethod");
+                   System.out.println("NestInnerMethod"+" - "+name);
                }
 
            }
 
         void  innerMethod(){
             System.out.println("inner class method "+name);
+            outerMethod();
         }
         Inner(){       //created a constructor
             System.out.println("inner constructor");
@@ -27,11 +29,13 @@ public class Nestedclasses {
      public static void main(String...args){
          Nestedclasses outerobj=new Nestedclasses();
          //Inner innerobj1=outerobj.new Inner();    //this one way we call the method 
-         outerobj.outerMethod();
-         Nestedclasses.Inner innerobj=new  Nestedclasses(). new Inner(); //calling a inner method from static method 
-         innerobj.innerMethod();
-         Nestedclasses.Inner.NestInner nestobj= /*new Nestedclasses(). new Inner()*/innerobj.new NestInner(); //calling nestinner one 
+        // outerobj.outerMethod();
+        // Nestedclasses.Inner innerobj=new Nestedclasses(). new Inner(); //calling a inner method from static method 
+         //innerobj.innerMethod();
+         Nestedclasses.Inner.NestInner nestobj= new Nestedclasses(). new Inner().new NestInner(); //calling nestinner one 
          nestobj.NestInnerMethod();
+        // nestobj.o
+        // nestobj.na
      }
 
 }
--- Manoj/src/Praticenew/Nested2.java
+++ Manoj/src/Praticenew/Nested2.java
@@ -3,21 +3,25 @@ package Praticenew;
 public class Nested2 {
     int outer_x=45;
     String name="Manoj";
+    static int y=100;
 
-     class Inner{      //created inner classs 
+     class Inner{      //created inner classs
+        static int j=10; 
          void display(){
-             System.out.println("display :outer_x"+" - "+outer_x+" "+name);
+             System.out.println("display :outer_x"+" - "+outer_x+" "+name+" "+y);
          }
 
      }
      void outer(){
-         Inner innerobj=new Inner(); //callled the inner one
+         Inner innerobj=new Inner(); //called the inner one
          innerobj.display();
 
      }
      public static void main(String...args){
-         Nested2 obj=new Nested2();   //called the outer one 
-         obj.outer();
+        // Nested2 obj=new Nested2();   //called the outer one 
+         //obj.outer();
+         Nested2.Inner obj2=new Nested2().new Inner();
+         obj2.display();
 
      }
     
