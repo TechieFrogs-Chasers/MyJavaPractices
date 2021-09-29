6a8dee2eaa321b6b7b2576dd6c9c2af2641250e4
manojbellapukonda <manojbellapukonda@gmail.com>
2021-09-29 23:22:19 +0530

    commit

--- Manoj/src/Nested3.java
+++ Manoj/src/Nested3.java
@@ -1,12 +1,16 @@
 public class Nested3 {
-   int m=45;
+   int m=5;
    int n=99;
+   static int f=5;
+   private String name="java";
      
      void outer(){
          for(int i=0; i<5;i++){ //inner class within a for loop
          class inner {       //method local inner class
+            int h=6;
+            int d=30;
              void inneri(){
-                 System.out.println("manoj"+ "- "+m);
+                 System.out.println("manoj"+ "- "+m+"*"+h+"="+d);
              }
          }
          inner innerobj=new inner();//for loocal inner class we able to call that class in method block
@@ -17,7 +21,7 @@ public class Nested3 {
         if (m<n){    // inner class with if condition 
         class inner2{      //method local inner class 
             void innerj(){
-                System.out.println("manoj"+"- "+n);
+                System.out.println("manoj"+"- "+n+" "+f+" "+name);
             }
         }
         inner2 inner2obj=new inner2(); //here we call the local inner class 
