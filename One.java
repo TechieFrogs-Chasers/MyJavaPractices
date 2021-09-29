c6559c7966796e34a6ab7a20d5add1dbe8007671
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-09-29 08:03:16 +0530

    Practice

--- One.java
+++ One.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class One {
+/*public class One {
     int id;
     String name;
     int age;
@@ -38,4 +38,76 @@ public static void main(String[] args) {
     }
 
   
+}*/
+
+/*public class PracticePrg
+{  
+    public static void main(String[] args) 
+    {
+        PracticePrg x = new PracticePrg("sindhu");
+    
+        x.evenOrOdd(5);
+
+    }
+
+    PracticePrg(String name)
+    {      //printing two constructors at a time by using this keyword
+        this(4);
+        System.out.println("default constructor");
+    }
+
+    PracticePrg(int y)
+    {
+        System.out.println("first constructor");
+
+    }
+
+     void evenOrOdd(int x)
+     {
+        if(x%2==0)
+        {
+            System.out.println("even number");
+        }
+        else 
+        {
+            System.out.println("odd number");
+        }
+
+    }
+}*/
+
+
+public class One{
+public static void main(String[] args) 
+{
+        One x = new One("sindhu");
+    
+       System.out.println(x.evenOrOdd(5));
+
+}
+
+    One(String name)
+    {      //printing two constructors at a time by using this keyword
+        this(4);
+        System.out.println("default constructor");
+    }
+
+    One(int y)
+    {
+        System.out.println("first constructor");
+
+    }
+
+     String evenOrOdd(int x)
+     {
+        if(x%2==0)
+        {
+            return "even number";
+        }
+        else 
+        {
+             return "odd number";
+        }
+     }
+
 }
