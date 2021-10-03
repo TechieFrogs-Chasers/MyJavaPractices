06c061603c1874485c15f433ef01d605e5ca15d0
Divysara <divyas94@gmail.com>
2021-10-03 18:01:34 -0500

    assignment3

---
+++ Dhivya/src/assignment3/Six.java
@@ -0,0 +1,23 @@
+package assignment3;
+
+public class Six {
+    
+}
+class Shape{
+
+}
+class Rectangle1 extends Shape{
+void display(){
+    System.out.println("This is Rectangle shape");
+}
+class Square{
+    void printMethod(){
+System.out.println("Square is a Rectangle");
+    }
+}
+}
+class Circle extends Shape{
+void method(){
+    System.out.println("This is Circle shape");
+}
+}
