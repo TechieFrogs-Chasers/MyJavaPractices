42c961be13f704c0f5c83d7f18e5b29751ac597a
Tulasikakara <tulasi.polina@gmail.com>
2021-10-12 11:01:20 -0400

    CHANGES DONE

--- Tulasi-Java/src/Practices/Interface_marketing.java
+++ Tulasi-Java/src/Practices/Interface_marketing.java
@@ -6,15 +6,33 @@ public class Interface_marketing implements Interface {
     public void valuemethod() {
         System.out.println("ValueMethod");
     }
+
     void method2() {
         System.out.println("method2");
     }
 
-    public  class Interface_sales extends Interface_marketing {
+    interface forMethod {
+        void forMethod();
+
+
+    }
+
+    public static class Interface_sales extends Interface_marketing implements forMethod {
         void method() {
             System.out.println("method");
         }
 
+
+        @Override
+        public void forMethod() {
+            System.out.println("forMethod");
+        }
+    }
+
+    public void defaultMethod() {//Override default Method- If we get error about in reduce visibility,means its a overridition method.On both inheritance and interface we can over ride the methods.
+        System.out.println("Override default Method ");//Overrider method will executed,first it will check in its class,then it will check interface
+        Interface.super.defaultMethod();//to call method if more methods with same name.
+        //InterfaceDemo.super.defaultMethod();//this is a class so we cant access in interface
     }
 
     public static void main(String[] args) {//main method
@@ -25,10 +43,18 @@ public class Interface_marketing implements Interface {
             }
         };
         int marketing = 99;
-        Interface_marketing obj2=new Interface_marketing() ;
+        Interface_marketing obj2 = new Interface_marketing();
+        Interface_sales obj5 = new Interface_sales();
+        obj5.forMethod();
+        obj5.defaultMethod();
+        obj5.method();
         obj.valuemethod();
         obj2.valuemethod();
         obj2.method2();
+
         System.out.println("Market value : " + (marketing + (float) 5.3));
+        // obj.super.defaultMethod();
+
+
     }
 }
