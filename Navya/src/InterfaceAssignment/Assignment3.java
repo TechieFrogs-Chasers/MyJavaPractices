eb6468674904095b25dd3435b259b194df59a93b
Navya-06 <chowdarynavya1999@gmail.com>
2021-10-13 18:32:51 -0400

    Practice And assignment

---
+++ Navya/src/InterfaceAssignment/Assignment3.java
@@ -0,0 +1,26 @@
+package InterfaceAssignment;
+public interface Assignment3{
+   void method1();
+   void method2();
+   void method3();
+}
+class Assignment implements Assignment3{
+
+    @Override
+    public void method1() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void method2() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void method3() {
+        // TODO Auto-generated method stub
+        
+    } 
+}
