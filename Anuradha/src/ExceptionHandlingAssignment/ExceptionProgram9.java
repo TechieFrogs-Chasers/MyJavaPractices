659f8fbc45c1a3d7d6bca64d1ea7c5530136ca8d
AnuradhaKaruturi <anuradhakaruturi.ak@gmail.com>
2021-10-16 06:22:15 -0600

    Exception Exercises

---
+++ Anuradha/src/ExceptionHandlingAssignment/ExceptionProgram9.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class ExceptionProgram9 {
+    public static void main(String[] args) throws FileNotFoundException,ClassNotFoundException{
+        try {
+            FileInputStream file = new FileInputStream("/src/Anuradha/abc.txt");
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("File not Found");
+          
+        }
+        try {
+            Class xyz = Class.forName("abc");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not Found");
+            
+        }
+           
+    }
+    
+}
