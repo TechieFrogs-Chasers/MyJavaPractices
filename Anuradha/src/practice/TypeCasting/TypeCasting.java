1fd09e42dce530f8a5db87585d81c17080212e7d
AnuradhaKaruturi <anuradhakaruturi.ak@gmail.com>
2021-10-20 06:42:11 -0600

    Type Casting Practice

---
+++ Anuradha/src/practice/TypeCasting/TypeCasting.java
@@ -0,0 +1,15 @@
+package TypeCasting;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        int num = 10;
+        float side = 20f;
+        double  area = num; //implicit casting
+
+        int number = (int) side; //explicit casting
+
+        int sum = (int)(10+20f+30.0);
+    }
+    
+}
