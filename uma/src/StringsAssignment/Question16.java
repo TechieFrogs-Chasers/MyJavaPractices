764eee3572b2e334e706cb522ae8e4d9af3dde49
umaboppana <mrsumaram@gmail.com>
2021-10-21 12:37:47 -0500

    done

---
+++ uma/src/StringsAssignment/Question16.java
@@ -0,0 +1,12 @@
+public class Question16 {
+    
+    public static void main(String[] args) {
+        String s= "welcome to the world";
+
+        String s1 = new String("welcome to the world");
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s1.toUpperCase());
+        
+    }
+}
