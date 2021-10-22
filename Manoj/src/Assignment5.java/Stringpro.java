e1835e5875924cb2d4443a2ee1eb76e05b8388a9
manojbellapukonda <manojbellapukonda@gmail.com>
2021-10-23 01:53:11 +0530

    commit

---
+++ Manoj/src/Assignment5.java/Stringpro.java
@@ -0,0 +1,19 @@
+package Assignment5.java;
+
+import javax.sound.midi.SysexMessage;
+
+public class Stringpro {
+    public static void main(String...args){
+    String s1="MANOJ";
+    StringBuilder s2=new StringBuilder ("Manoj");
+    s1=s1.toLowerCase();
+    System.out.println(s1);
+    s1=s1.toUpperCase();
+    System.out.println(s1);
+
+    
+
+    System.out.println(s2.reverse());//reverse method is only used for modified strings
+    
+    } 
+}
