190b474e9c598e464f9d2a278df3bc5574b36911
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-10-22 17:03:34 +0530

    commit

---
+++ Srikanth/src/practice.java/Ternary.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 15;
+
+        int c = (a < b) ? a : b;
+        System.out.println(c);
+
+    }
+}
