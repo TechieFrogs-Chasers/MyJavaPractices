a9fc5aa139b613e83a70726da786b7dfbf77a6a5
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-10-27 05:25:50 +0530

    Arrays

--- sindhu/src/ArraysAssignment/Fifth.java
+++ sindhu/src/ArraysAssignment/Fifth.java
@@ -1,6 +1,14 @@
 package sindhu.src.ArraysAssignment;
 
 public class Fifth {
-    
+    public static void main(String[] args) {
+
+        int[] x = {2,3,4,5,6,67,8};
+        for(int i=0; i<x.length;i++){
+            System.out.println(x[i]);
+        }
+        
+    }
+
     
 }
