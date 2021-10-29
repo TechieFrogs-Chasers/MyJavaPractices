90cbee08537970649566ed5242f8273344a2f7fc
Battineni-H <hemabattineni14@gmail.com>
2021-10-29 11:27:36 -0500

    hemalatha

--- hemalatha/src/Arrays/Ascending.java
+++ hemalatha/src/Arrays/Ascending.java
@@ -4,13 +4,13 @@ public class Ascending {
     public static void main(String[] args) {
         int a[] = new int[]{9,4,6,1,8,0,2,5};
          int temp=0;
-         System.out.println("before sorting elements in the array ");
+         System.out.println("before sorting elements in the array:");
             for(int i=0;i<a.length;i++){
              System.out.print(a[i]+ " ");
             }
          
              for(int i=0;i<a.length;i++){
-                 for(int j=i+1;j<a.length;j++){
+                 for(int j=i+1;j<a.length;j++){   
                      if(a[i]>a[j]){
                      temp=a[i];
                      a[i]=a[j];
@@ -19,7 +19,7 @@ public class Ascending {
                 }
              }
                  System.out.println(" ");
-                    System.out.println("after sorting elements in array");
+                    System.out.println("after sorting elements in array:");
                         for(int i=0;i<a.length;i++){
                             System.out.print(a[i]+ " ");
                         }
