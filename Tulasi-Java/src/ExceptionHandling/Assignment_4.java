d83fd0791685f4ddc6cd3d99c076a84efce41bb0
Tulasikakara <tulasi.polina@gmail.com>
2021-10-30 22:17:43 -0400

    CHANGES DONE

--- Tulasi-Java/src/ExceptionHandling/Assignment_4.java
+++ Tulasi-Java/src/ExceptionHandling/Assignment_4.java
@@ -2,15 +2,23 @@ package ExceptionHandling;
 
 import java.util.Scanner;
 
-public class Assignment_4 {
+public class Assignment_4 {//this assignment connected to assignment-16 for clonenotsupportexception.
     public static void main(String[] args) {
         Scanner scannerObj=new Scanner(System.in);
         System.out.println("Enter numbers: ");
         int num=scannerObj.nextInt();
         int num1=scannerObj.nextInt();
         methodOfThrows(num,num1);
+
         scannerObj.close();
 
+    }
+    Assignment_4 (int num,int num1){
+        System.out.println("Constructor that connected to clone assignment-16");
+
+    }
+    public Object clone()throws CloneNotSupportedException{
+        return super.clone();
     }
     public static void methodOfThrows(int num,int num1) throws ArithmeticException{
         if(num1<0){
