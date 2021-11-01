6aca6beaf3f05343f78c7f93af9deb477c4b30ad
sindhupsvn <sindhuchowdary813@gmail.com>
2021-11-01 13:25:13 +0530

    throws

---
+++ sindhu/src/Exceptions/throws2.java
@@ -0,0 +1,69 @@
+package Exceptions;
+
+public class throws2 {
+
+    void m1() throws ArithmeticException,ArrayIndexOutOfBoundsException,NegativeArraySizeException{
+
+     //void m1(){
+
+        try{
+            throw new ArithmeticException("not valid condition");
+        }
+        catch(ArithmeticException ae){
+            ae.printStackTrace();
+
+            System.out.println("custom message");
+        }
+        
+    }
+        
+    throws2(String name){
+
+        System.out.println("constructor1");
+    }
+
+
+    void m2(){
+
+        try{
+            //System.exit(0);
+
+            throw new ArrayIndexOutOfBoundsException(); //anonymus way 
+        }
+        catch(ArrayIndexOutOfBoundsException ai){
+
+            System.out.println("custom message2");
+        }
+        finally{
+            System.out.println("custom message 3");
+        }
+    }
+
+    void m3(){
+        
+
+        try{
+            throw new NegativeArraySizeException();
+        }
+        catch(NegativeArraySizeException n){
+
+            System.out.println("custommessage4");
+            
+        }
+    }
+
+    throws2(){
+        
+        this("sindhu");
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        
+        throws2 x = new throws2();
+        x.m1();
+        x.m2();
+        x.m3();
+    }
+    
+}
