abc4cda944e4eacd15c5b87ab33a3c643f0dbf2a
AnuradhaKaruturi <anuradhakaruturi.ak@gmail.com>
2021-11-03 22:32:28 +0530

    changes

--- Anuradha/src/practice/Dates/LocalDates.java
+++ Anuradha/src/practice/Dates/LocalDates.java
@@ -10,8 +10,6 @@ public class LocalDates {
         LocalDate myObj = LocalDate.now(); // Create a date object
         System.out.println(myObj); // Display the current date
 
-        System.out.println();
-
         LocalTime myObj1 = LocalTime.now(); //create a time object
         System.out.println(myObj1);//display the current time
 
