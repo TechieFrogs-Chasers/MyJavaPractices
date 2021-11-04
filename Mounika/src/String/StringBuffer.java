e2ed90e308469820dc9cacf723c9892a32864ffe
mounikanadella61@gmail.com <mounikanadella61@gmail.com>
2021-11-04 14:32:41 -0400

    changes

---
+++ Mounika/src/String/StringBuffer.java
@@ -0,0 +1,9 @@
+public class StringBuffer {
+    
+    public static void main(String[] args) {
+
+        StringBuffer stBuff = new StringBuffer();
+            System.out.println(stBuff.capacity());
+        
+    }
+}
